edc636ff4cb1d4c42f386aab08d833740c4db797
Matt Garner <matt.j.garner@gmail.com>
2014-11-14 01:37:03 -0800

    Added docstrings for all 'completed' functions

--- parse_LRG.py
+++ parse_LRG.py
@@ -1,6 +1,6 @@
 class LRG(object):
             
-    def __init__(self, filepath='C:/lrg_xml_parser/LRG_292.xml'):
+    def __init__(self, filepath='LRG_292.xml'):
         import xml.etree.ElementTree as etree
         self.xmlfile = filepath
         self.tree = etree.parse(self.xmlfile)
@@ -9,9 +9,11 @@ class LRG(object):
         self.sequences = self.set_sequences()
         self.exons = self.set_exons()
         #self.references = self.set_references()
-
+        self.set_exon_seq()
 
     def set_id(self):
+        '''Returns LRG id string from LRG xml
+            e.g. "LRG_292"          '''
         LRG_ids = self.root.findall("./fixed_annotation/id")
         for LRG in LRG_ids:
             LRG_id = LRG.text
@@ -19,6 +21,9 @@ class LRG(object):
                 
 
     def set_sequences(self):
+        '''Returns dictionary containing sequence ids as keys, sequences as values from LRG xml
+        Includes genomic, transcript, translation
+        e.g. {"LRG_292":"ATCG....", LRG_292t1:"ATCG....", LRG292p1:"AACE"}'''
         sequences = {}
         sequences.update(self.set_genomic_seq())
         sequences.update(self.set_cDNA())
@@ -27,6 +32,9 @@ class LRG(object):
 
         
     def set_genomic_seq(self):
+        '''Returns dictionary containing sequence ids as keys, sequences as values from LRG xml
+        Genomic only
+        e.g. {"LRG_292":"ATCG...."}'''
         genomic_id_seq = {}
         for item in (items for items in self.root[0] if items.tag == 'sequence'):
             genomic_seq = item.text
@@ -37,6 +45,9 @@ class LRG(object):
 
 
     def set_cDNA(self):
+        '''Returns dictionary containing sequence ids as keys, sequences as values from LRG xml
+        Transcripts of genomic sequence only
+        e.g. {"LRG_292t1":"ATCG...."}'''
         transcripts = {}
         transcript_elems = self.root.findall("./fixed_annotation/transcript")
         for transcript in transcript_elems:
@@ -48,6 +59,9 @@ class LRG(object):
 
 
     def set_protein(self):
+        '''Returns dictionary containing sequence ids as keys, sequences as values from LRG xml
+        Proteins from genomic transcripts only
+        e.g. {"LRG_292p1":"AACE...."}'''
         proteins = {}
         protein_elems = self.root.findall("./fixed_annotation/transcript/coding_region/translation")
         for protein in protein_elems:
@@ -61,6 +75,10 @@ class LRG(object):
 
 
     def set_exons(self):
+        '''Returns dictionary containing exon numbers as keys, dictionary as values.
+           Nested dictionary contains reference sequence as keys, dictionary as values
+           Second nested dictionary contains "Start","End" as keys,
+               coordinates within parent reference seq as values'''
         exons = {}
         for exon in self.tree.iter(tag = 'exon'):
             if 'label' in exon.attrib:
@@ -82,7 +100,10 @@ class LRG(object):
 
 
     def set_references(self):
-
+        '''Returns list of reference sequence names
+           for genomic, transcript, translation sequences
+           Not currently used as redundant (use self.sequences.keys() instead)'''
+    
         for item in self.tree.iter(tag = 'id'):
             genomic_id = item.text
 
@@ -104,14 +125,19 @@ class LRG(object):
         return references
 
     def get_exon_list(self):
+        '''Prints a list of  sorted exon numbers and sequenes they map to within the LRG xml
+           E.g. "2 ['LRG_292', 'LRG_292t1', 'LRG_292p1']"    '''
+
         sorted_exon_list = sorted(map(int, self.exons.keys()))
-        print sorted_exon_list
         for exon in sorted_exon_list:
             exon_refs = self.exons[str(exon)].keys()
-        
-            print exon, exon_refs
+            print "Exon:", exon, exon_refs
             
-    def get_exon_seq(self, exon_list = None, upstream = 0, downstream = 0):
+    def set_exon_seq(self, exon_list = None, upstream = 0, downstream = 0):
+        '''Retrieves the exon sequence for each exon/reference sequence combination
+           Saves the sequence in the second nested dictionary in self.exons
+               i.e. in addition to keys "Start", "End"
+                 now also contains {Sequence":exon sequence} '''
         if exon_list == None:
             exon_list = self.exons.keys()
             sorted_exon_list = sorted(map(int, exon_list))
@@ -119,7 +145,7 @@ class LRG(object):
 
         for reference in self.sequences.keys():
             print "@@@@@@@@@@@@@@@@@@@@@@@@\n@@@@@@@@@@@@@@@@@@@@@@@@"
-            print "\n\nRef:     ", reference
+            print "\n\nRef:     ", reference, "\n"
             for exon in sorted_exon_list:
                 exon = str(exon)
                 print "Ref:     ", reference
